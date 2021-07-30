5fa3fa22c643760eb6f70a70de25f1a282cf2f55
Chirag Jaju <76174447+ChiragJaju@users.noreply.github.com>
2021-07-30 21:45:47 +0530

    Update v2,1

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const _ = require('lodash');
 const ejs = require("ejs");
-const posts = [];
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect("mongodb+srv://<USER ID>:<PASSWORD>@cluster0.phqu2.mongodb.net/BlogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+const blogSchema = new mongoose.Schema({
+  name: String,
+  content: String,
+  post_name: String
+});
+
+const Blog = new mongoose.model("blog", blogSchema);
+
+
+const home = new Blog({
+  name: "Home",
+  post_name: "home",
+  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+});
+
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+
+
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
+
+
+
+
+
+
 
 const app = express();
 
@@ -17,10 +45,26 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
-  });
+  Blog.find(function (err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (results.length === 0) {
+        // 
+        home.save()
+        res.redirect('/');
+      } else {
+        res.render('home', {
+          results: results
+        })
+
+      }
+    }
+
+  })
+
+
+
 });
 
 
@@ -46,29 +90,58 @@ app.post('/compose', (req, res) => {
     postBody: req.body.textBody,
     postTitle: req.body.textTitle,
   };
-  posts.push(post);
-  res.redirect("/");
+  const blog = new Blog({
+    name: post.postTitle,
+    content: post.postBody,
+    post_name: _.lowerCase(post.postTitle)
+  })
+  console.log(_.lowerCase(post.postTitle));
+  if (post.postBody.length === 0 || post.postTitle.length === 0) {
+    res.redirect("/");
+  } else {
+    blog.save();
+    res.redirect("/");
+
+  }
 
 
 });
+
+
 
 app.get('/posts/:postId', function (req, res) {
-  let requestedTitle = _.lowerCase(req.params.postId);
-  posts.forEach(function (posts) {
-    let storedTitle = _.lowerCase(posts.postTitle);
+  // let requestedTitle = _.lowerCase(req.params.postId);
 
-    if (storedTitle === requestedTitle) {
-      let postBody = posts.postBody;
+  Blog.findOne({
+    post_name: _.lowerCase(req.params.postId)
+  }, function (err, results) {
+    if (err) {
+      console.log(err);
+    } else {
       res.render('post', {
-        postTitle: storedTitle,
-        postBody: postBody
+        postTitle: results.name,
+        postBody: results.content,
+        post_nameId: results.post_name
       });
     }
-  });
+  })
+
 
 });
 
+app.post('/posts/:postId', function (req, res) {
+  let requestedTitle = _.lowerCase(req.params.postId);
 
-app.listen(3000, function () {
+  Blog.deleteOne({
+    post_name: requestedTitle
+  }, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
